0111330317fdebc6416ab6f0e4bef37bbd638fba
brimends <brimends@yahoo.com>
2024-02-18 22:40:25 +0000

    Adding image to the home page

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
+
+
+
 import streamlit as st
-
-
 st.set_page_config(
     page_title='Home',
     page_icon= '',
     layout= 'wide'
 )
+
+st.title('My Customer Churn App')
+
+from PIL import Image
+Img = Image.open('voda pix.png')
+
+st.image(
+    Img,
+
+    width=700
+
+
+)
+
+
+
+
+
+
 
 
---
+++ pages/2_Dashboard.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title='Dashboard',
+    page_icon= '',
+    layout= 'wide'
+)
