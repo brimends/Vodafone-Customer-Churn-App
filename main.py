1dec5a3acf3a64d72921d1f8afda7c1eba4e808e
Solomon Yeboah <modelmaestrosolo@gmail.com>
2024-02-20 15:55:42 +0000

    Update main.py
    include key features and instruction sections to the main page. for easy navigation by users

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
-
-
-
 import streamlit as st
-st.set_page_config(
-    page_title='Home',
-    page_icon= '',
-    layout= 'wide'
-)
 
-st.title('My Customer Churn App')
+st.set_page_config(page_title="Home", page_icon="", layout="wide")
+
+st.title("My Customer Churn App")
 
 from PIL import Image
-Img = Image.open('voda pix.png')
 
-st.image(
-    Img,
+Img = Image.open("voda pix.png")
 
-    width=700
+st.image(Img, width=700)
 
+tabl, tab2 = st.columns(2)
 
-)
+with tabl:
+    st.subheader("Key Features")
+    st.markdown(
+        """
+                - Data view
+                - A prediction page
+                - A dashboard for EDA and KPI monitoring
+                
+                """
+    )
 
-
-
-
-
-
-
-
+with tab2:
+    st.subheader("Usage Instructions")
+    st.markdown(
+        """
+                - Supply values for inputs
+                - Hit the predict button
+                - Your prediction will be displayed after a few minutes!
+                
+                """
+    )
